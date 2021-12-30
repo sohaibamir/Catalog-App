e1df118e86584bf256abbdbd7599ff530fbfc3de
Sohaib Bin Amir <sohaib.binamir16@gmail.com>
2021-12-30 13:08:58 +0500

    First commit

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("My First App"),
+          backgroundColor: Colors.black26,
+        ),
+        body: const Center(
+          child: Center(
+            child: Text("Hi Flutter"),
+          ),
+        ),
+        bottomNavigationBar: const Text(
+          "End of the app",
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+
+void main() {
+  //runApp(const MaterialApp(home: Text("Hello")));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
