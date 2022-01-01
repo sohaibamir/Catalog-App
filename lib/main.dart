17791224a90c697f74f393630fc6720282523835
Sohaib Bin Amir <sohaib.binamir16@gmail.com>
2022-01-02 01:51:19 +0500

    completed day 4

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   //runApp(const MaterialApp(home: Text("Hello")));
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // add();
@@ -18,8 +21,12 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: GoogleFonts.lato().fontFamily),
       initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,20 +3,78 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("My First App"),
-          backgroundColor: Colors.black26,
-        ),
-        body: const Center(
-          child: Center(
-            child: Text("Hi Flutter"),
-          ),
-        ),
-        bottomNavigationBar: const Text(
-          "End of the app",
-          textAlign: TextAlign.center,
+    return Material(
+        color: Colors.grey.shade300,
+        child: Column(
+          children: [
+            // ignore: prefer_const_constructors
+            SizedBox(
+              height: 30.0,
+            ),
+            Image.asset(
+              "assets/images/random.jpg",
+              fit: BoxFit.cover,
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(
+              height: 20.0,
+            ),
+            // ignore: prefer_const_constructors
+            Text(
+              "Welcome",
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: Column(
+                  children: [
+                    TextFormField(
+                        cursorColor: Colors.black,
+                        style: const TextStyle(color: Colors.black),
+                        decoration: const InputDecoration(
+                            hintText: "Enter username",
+                            labelText: "Username",
+                            hintStyle: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.w200),
+                            labelStyle: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold))),
+                    TextFormField(
+                        obscureText: true,
+                        cursorColor: Colors.black,
+                        style: const TextStyle(color: Colors.black),
+                        decoration: const InputDecoration(
+                            hintText: "Enter password",
+                            labelText: "Password",
+                            hintStyle: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.w200),
+                            labelStyle: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold))),
+                    // ignore: prefer_const_constructors
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // ignore: avoid_print
+                        print("Button working");
+                      },
+                      child: const Text("Login"),
+                      style: TextButton.styleFrom(primary: Colors.black),
+                    )
+                  ],
+                )),
+          ],
         ));
   }
 }
